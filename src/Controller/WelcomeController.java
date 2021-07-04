f1b1fbe0b6b8ee0ebe0cb0f10b1b355fe6be68b3
AshKalor <akhibkalnoor@gmail.com>
2021-07-04 23:21:28 +0530

    Add Controller for Welcome.fxml

--- src/Controller/WelcomeController.java
+++ src/Controller/WelcomeController.java
@@ -1,10 +1,26 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class WelcomeController {
-
-    public void welcomeHandler(MouseEvent event){
-        System.out.println(event);
+    public Label welcomeText;
+    private Scene scene;
+    private Stage stage;
+    private Parent root;
+    public void welcomeHandler(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../Views/MainMenu.fxml")));
+        stage= (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
