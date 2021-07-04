7d99039eb17b7bba7fbd5919cffa49665c5ba0e1
AshKalor <akhibkalnoor@gmail.com>
2021-07-04 22:32:23 +0530

    Initial Commit create basic project structure

---
+++ src/Main/Main.java
@@ -0,0 +1,29 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../Views/Welcome.fxml")));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 800, 600));
+            primaryStage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
