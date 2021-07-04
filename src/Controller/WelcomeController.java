141f206b8e2fbbae6cb0c629c856b8bd733a3656
AshKalor <akhibkalnoor@gmail.com>
2021-07-04 22:35:10 +0530

    Add the initial fxml files

---
+++ src/Controller/WelcomeController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class WelcomeController {
+
+    public void welcomeHandler(MouseEvent event){
+        System.out.println(event);
+    }
+}
